5e183f94b3a1d8cff99cb29dc991c2e5adb4cf04
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-03 19:22:16 -0400

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+import footer from '../styles/Footer.module.scss'
+
+export default function Footer() {
+    return <footer className={footer.footer}>&copy; Secondhand Literature 2021, Designed by Paul Serbanescu</footer>;
+}
