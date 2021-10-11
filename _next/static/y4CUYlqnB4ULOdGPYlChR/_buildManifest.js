07a687e97faf13689b7fe01fedd573bc35ed191b
pserb <paulserbanescu3@gmail.com>
2021-10-11 20:37:51 +0000

    Deploying to gh-pages from @ SecondHandLiterature/secondhandliterature.org@4ba73a58038155e911b7fef184fb740e33eb0e68 🚀

---
+++ _next/static/y4CUYlqnB4ULOdGPYlChR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/132e451916dc54d1df11.css","static/chunks/pages/index-b9ac32892815f24859e6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
