1ee87b6bb39df56e103e64406ac8e05364268710
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-25 03:38:21 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -38,8 +38,7 @@ export default function Navbar({ }) {
       <Link className="main-link" href="/about"><a>About Us</a></Link>
       <Link className="main-link" href="/howitworks"><a>How it Works</a></Link>
       <Link className="main-link" href="/board"><a>Meet the Board</a></Link>
-      <Link className="main-link" href="/join"><a>Join the Team</a></Link>
-      <Link className="main-link" href="/"><a>Contact</a></Link>
+      <Link className="main-link" href="/contact"><a>Contact</a></Link>
       <div className={navbar.socials}>
         <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a>
         <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a>
@@ -58,7 +57,6 @@ export default function Navbar({ }) {
             <Link className="main-link" passHref href="/about"><li><a onClick={toggle}>About Us</a></li></Link>
             <Link className="main-link" passHref href="/howitworks"><li><a onClick={toggle}>How it Works</a></li></Link>
             <Link className="main-link" passHref href="/board"><li><a onClick={toggle}>Meet the Board</a></li></Link>
-            <Link className="main-link" passHref href="/join"><li><a onClick={toggle}>Join the Team</a></li></Link>
             <Link className="main-link" passHref href="/"><li><a onClick={toggle}>Contact</a></li></Link>
             <li className={navbar.socials}>
               <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a>
