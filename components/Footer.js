05c78ba5589aac96ae44a7140481103cc500d66e
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-30 18:35:34 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import footer from '../styles/Footer.module.scss'
 
 export default function Footer() {
-    return <footer className={footer.footer}>&copy; Secondhand Literature 2021, Designed by Paul Serbanescu</footer>;
+    return <footer className={footer.footer}>&copy; Secondhand Literature 2021, Designed by&nbsp;<a href="https://github.com/pserb">Paul Serbanescu</a></footer>;
 }
