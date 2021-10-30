accf653d799967e3f723d5e05f76730f2c8c0319
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-30 18:35:39 -0400

    Update board.js

--- pages/board.js
+++ pages/board.js
@@ -21,16 +21,17 @@ export default function Board() {
           <h2>Taee Chi</h2>
           <img src={getImg("/taeeGOOD.jpg")} />
           <p>
-            Hey everyone! My name is Taee, and I&apos;m a junior currently attending
-            Stuyvesant High School. I&apos;m from Flushing, Queens, and I co-founded
-            Second Hand as a way of sharing my love for reading with others in
-            my community. I am so excited to work with my talented board members
-            to achieve this goal, and I hope you can help us too by donating a
-            book! If you couldn&apos;t tell already, I like to read in my free time,
-            but I also enjoy playing handball (American, not European) and
-            watching movies. The most recent book I&apos;ve read is Patron Saints of
-            Nothing, which is about a Filipino-American teenager uncovering the
-            truth about his cousin&apos;s death (9/10, would recommend!){" "}
+            Hey everyone! My name is Taee, and I&apos;m a junior currently
+            attending Stuyvesant High School. I&apos;m from Flushing, Queens,
+            and I co-founded Second Hand as a way of sharing my love for reading
+            with others in my community. I am so excited to work with my
+            talented board members to achieve this goal, and I hope you can help
+            us too by donating a book! If you couldn&apos;t tell already, I like
+            to read in my free time, but I also enjoy playing handball
+            (American, not European) and watching movies. The most recent book
+            I&apos;ve read is Patron Saints of Nothing, which is about a
+            Filipino-American teenager uncovering the truth about his
+            cousin&apos;s death (9/10, would recommend!){" "}
           </p>
         </div>
         <div className={board.special}>
@@ -38,8 +39,8 @@ export default function Board() {
           <img src={getImg("/josh.png")} />
           <p>
             Hello everyone, my name is Joshua and I&apos;m a junior at Townsend
-            Harris High School. When I&apos;m not in school, I spend most of my time
-            listening to music, and well, reading. I feel that co-founding
+            Harris High School. When I&apos;m not in school, I spend most of my
+            time listening to music, and well, reading. I feel that co-founding
             Second Hand has allowed me to take my passion for books and elevate
             it to something of greater importance. A book that reminds me of my
             childhood is The Giving Tree, as I vividly remember my parents
@@ -62,26 +63,26 @@ export default function Board() {
             <p>Social Media Director</p>
           </div>
           <p>
-            Hey everyone! I&apos;m Christina, currently a junior at Stuyvesant High
-            School. I am so excited to be part of Second Hand as a social media
-            director. I believe that this organization has great potential, and
-            I am looking forward to working with everyone! I recently finished
-            reading Interior Chinatown, a teleplay that points out the
-            stereotypes in our society as well as what defines one&apos;s identity.
-            In my free time, I love to dance, binge watch Cdramas, hang out with
-            friends, and try out new things.
+            Hey everyone! I&apos;m Christina, currently a junior at Stuyvesant
+            High School. I am so excited to be part of Second Hand as a social
+            media director. I believe that this organization has great
+            potential, and I am looking forward to working with everyone! I
+            recently finished reading Interior Chinatown, a teleplay that points
+            out the stereotypes in our society as well as what defines
+            one&apos;s identity. In my free time, I love to dance, binge watch
+            Cdramas, hang out with friends, and try out new things.
           </p>
         </div>
         <div>
           <p>
-            Hey guys! I&apos;m Rishima, and I&apos;m a junior at Townsend Harris High
-            School. I&apos;m super excited to be a Social Media Director for Second
-            Hand, and I am so honored to have been given this opportunity to
-            help others strengthen their connection to reading and find not just
-            a home but a community in literature. In my down time, I like to
-            read (obviously!), go out with my friends, and cook new things. I
-            recently read Lord of the Flies and would definitely recommend it if
-            you are looking for a thought-provoking classic.
+            Hey guys! I&apos;m Rishima, and I&apos;m a junior at Townsend Harris
+            High School. I&apos;m super excited to be a Social Media Director
+            for Second Hand, and I am so honored to have been given this
+            opportunity to help others strengthen their connection to reading
+            and find not just a home but a community in literature. In my down
+            time, I like to read (obviously!), go out with my friends, and cook
+            new things. I recently read Lord of the Flies and would definitely
+            recommend it if you are looking for a thought-provoking classic.
           </p>
           <div className={board.member}>
             <h2>Rishima Pokharel</h2>
@@ -98,26 +99,27 @@ export default function Board() {
           <p>
             Hello everyone! My name is Shafiul Haque and I&apos;m a junior at
             Stuyvesant High School. Some of my hobbies include reading, playing
-            sports, and biking. As the outreach director of Second Hand, I&apos;ll be
-            responsible for keeping in touch with our recipient locations and
-            partner organizations. I am very excited to be a part of Second Hand
-            because I believe that our community will benefit tremendously from
-            the outpour of books. Personally, I love reading fantasy and
-            adventure books, and I&apos;m currently reading the City of Fallen
-            Angels, the fourth book in the Mortal Instrument series. The book is
-            about a girl who discovers her own heritage and her family history
-            in the world of magic.{" "}
+            sports, and biking. As the outreach director of Second Hand,
+            I&apos;ll be responsible for keeping in touch with our recipient
+            locations and partner organizations. I am very excited to be a part
+            of Second Hand because I believe that our community will benefit
+            tremendously from the outpour of books. Personally, I love reading
+            fantasy and adventure books, and I&apos;m currently reading the City
+            of Fallen Angels, the fourth book in the Mortal Instrument series.
+            The book is about a girl who discovers her own heritage and her
+            family history in the world of magic.{" "}
           </p>
         </div>
         <div>
           <p>
-            Hey everyone! My name&apos;s Darian and I&apos;m a junior at Townsend Harris
-            High School. As Second Hand&apos;s volunteer coordinator, I am excited to
-            work with all of you and use my passion for volunteering to bring
-            books to those who need them. In my free time I enjoy playing music
-            and cubing. My favorite book of all time would have to be Prisoner
-            B-3087. It was one of the first books I read from cover to cover in
-            one weekend, and it provided a basis for my love of history.
+            Hey everyone! My name&apos;s Darian and I&apos;m a junior at
+            Townsend Harris High School. As Second Hand&apos;s volunteer
+            coordinator, I am excited to work with all of you and use my passion
+            for volunteering to bring books to those who need them. In my free
+            time I enjoy playing music and cubing. My favorite book of all time
+            would have to be Prisoner B-3087. It was one of the first books I
+            read from cover to cover in one weekend, and it provided a basis for
+            my love of history.
           </p>
           <div className={board.member}>
             <h2>Darian Jimenez</h2>
@@ -145,17 +147,13 @@ export default function Board() {
         </div>
         <div>
           <p>
-            Hello everyone! My name is Shafiul Haque and I&apos;m a junior at
-            Stuyvesant High School. Some of my hobbies include reading, playing
-            sports, and biking. As the outreach director of Second Hand, I&apos;ll be
-            responsible for keeping in touch with our recipient locations and
-            partner organizations. I am very excited to be a part of Second Hand
-            because I believe that our community will benefit tremendously from
-            the outpour of books. Personally, I love reading fantasy and
-            adventure books, and I&apos;m currently reading the City of Fallen
-            Angels, the fourth book in the Mortal Instrument series. The book is
-            about a girl who discovers her own heritage and her family history
-            in the world of magic.{" "}
+            Hey, I&apos;m Paul Serbanescu, and I&apos;m a junior at Stuyvesant High
+            School. I really enjoy coding and working on cs projects, listening
+            to music, and playing video games. I also enjoy reading of course!
+            As the webmaster, I&apos;m in charge of designing and running the Second
+            Hand website! My favorite book of all time is A Wrinkle in Time by
+            Madeleine L'Engle. Reading expands your imagination, and it&apos;s always
+            fun slipping into a fantasy world and being immersed in a story.
           </p>
           <div className={board.member}>
             <h2>Paul Serbanescu</h2>
